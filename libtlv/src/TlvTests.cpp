7d269eca84a85f827a1501e35914e08fba19e0a8
izaya0 <michalcitko95@gmail.com>
2017-06-03 10:07:14 +0000

    added more tests to libtlv

--- libtlv/src/TlvTests.cpp
+++ libtlv/src/TlvTests.cpp
@@ -10,6 +10,58 @@
 #include <iostream>
 #include <memory>
 
+
+BOOST_AUTO_TEST_CASE(getTagTest1) {
+	int tag = Tlv::getTag(1, 2, 3, 5);
+	int tag2 = 1 << 24 | 2 << 16 | 3 << 8 | 5;
+	BOOST_CHECK_EQUAL(tag, tag2); 
+	
+}
+
+BOOST_AUTO_TEST_CASE(getTagTest2) {
+	int tag = Tlv::getTag(8, 99, 31, 21);
+	int tag2 = 8 << 24 | 99 << 16 | 31 << 8 | 21;
+	BOOST_CHECK_EQUAL(tag, tag2); 
+}
+
+BOOST_AUTO_TEST_CASE(getTagTest3) {
+	int tag = Tlv::getTag(6, 5, 5, 5);
+	int tag2 = 6 << 24 | 5 << 16 | 5 << 8 | 5;
+	BOOST_CHECK_EQUAL(tag, tag2); 
+}
+
+BOOST_AUTO_TEST_CASE(getTagTestArray) {
+	unsigned char tag_array[4] = {55, 2, 100, 10};
+	int tag = Tlv::getTag(tag_array);
+	int tag2 = 55 << 24 | 2 << 16 | 100 << 8 | 10;
+	BOOST_CHECK_EQUAL(tag, tag2); 
+}
+
+BOOST_AUTO_TEST_CASE(getTagTestArray2) {
+	unsigned char tag_array[4] = {1, 2, 3, 10};
+	int tag = Tlv::getTag(tag_array);
+	int tag2 = 1 << 24 | 2 << 16 | 3 << 8 | 10;
+	BOOST_CHECK_EQUAL(tag, tag2); 
+}
+
+BOOST_AUTO_TEST_CASE(getTagArray_and_getTag) {
+	unsigned char tag_array[4] = {1, 6, 8, 10};
+	int tag0 = Tlv::getTag(1, 6, 8, 10);
+	int tag = Tlv::getTag(tag_array);
+	int tag2 = 1 << 24 | 6 << 16 | 8 << 8 | 10;
+	BOOST_CHECK_EQUAL(tag, tag0); 
+	BOOST_CHECK_EQUAL(tag2, tag0); 
+}
+
+BOOST_AUTO_TEST_CASE(getTagArray_and_getTag2) {
+	unsigned char tag_array[4] = {2, 200, 22, 10};
+	int tag0 = Tlv::getTag(2, 200, 22, 10);
+	int tag = Tlv::getTag(tag_array);
+	int tag2 = 2 << 24 | 200 << 16 | 22 << 8 | 10;
+	BOOST_CHECK_EQUAL(tag, tag0); 
+	BOOST_CHECK_EQUAL(tag2, tag0); 
+}
+
 BOOST_AUTO_TEST_CASE(normal_find) {
 	Tlv o;
 	unsigned char first_buffer[] = {3, 3, 3};
@@ -46,9 +98,10 @@ BOOST_AUTO_TEST_CASE(normal_find3) {
 	o.add(13, 0, boost::size(first_buffer), first_buffer);
 	o.add(14, 1, boost::size(second_buffer), second_buffer);
 
-	std::vector<unsigned char> buffer = o.getTagData(16909060);
+	std::vector<unsigned char> buffer = o.getTagData(Tlv::getTag(1, 2, 3, 4));
 	// 1, 2, 3, 4 is 00000001000000100000001100000100 which is 16909060 
 	// 1<<24 | 2<<16 | 3<<8 | 4
+	// which is what getTag returns
 	BOOST_REQUIRE_EQUAL(buffer.size(), 2); // check if data size == 2
 	BOOST_CHECK_EQUAL(buffer[0], 4); // check if data == 4, 4
 	BOOST_CHECK_EQUAL(buffer[1], 4);
@@ -125,20 +178,74 @@ BOOST_AUTO_TEST_CASE(parse_and_find) {
 }
 
 
-BOOST_AUTO_TEST_CASE(foo) {
-	unsigned char foo[] = { 0x11, 0x00, 0x00, 0x01, 0x00, 0x04, 0x00, 0x00, 0x00, 0x01 };
-	std::cout << "OK" << std::endl;
-	Tlv client_id_tag(foo, sizeof(foo));
-	std::cout << "OK" << std::endl;
+BOOST_AUTO_TEST_CASE(hex) {
+	unsigned char data[] = { 0x11, 0x00, 0x00, 0x01, 0x00, 0x04, 0x00, 0x00, 0x00, 0x01 };
+	Tlv client_id_tag(data, sizeof(data));
 
-	std::vector<unsigned char> client_id_vector = client_id_tag.getTagData(Tlv::getTag(0x11, 0x00, 0x00, 0x01));
-	std::cout << "OK" << client_id_vector.size() << std::endl;
+	std::vector<unsigned char> buffer = client_id_tag.getTagData(Tlv::getTag(0x11, 0x00, 0x00, 0x01));
 
-	int client_id;
-	for(auto it = client_id_vector.begin(); it != client_id_vector.end(); it++) {
-		std::cout << *it <<std::endl;
+	BOOST_REQUIRE_EQUAL(buffer.size(), 4); 
+	// 0x00 0x00 0x00 0x01 is data
+	
+	int j = 6;
+	for(auto i : buffer) {
+		BOOST_CHECK_EQUAL(i, data[j]);
+		++j;
 	}
-	memcpy(&client_id, &client_id_vector[0], sizeof(client_id));
-	BOOST_CHECK_EQUAL(client_id, 1);
 }
+
+BOOST_AUTO_TEST_CASE(hex2) {
+	unsigned char data[] = { 0x23, 0x00, 0x00, 0x02, 0x00, 0x02, 0x00, 0x00};
+	Tlv client_id_tag(data, sizeof(data));
+
+	std::vector<unsigned char> buffer = client_id_tag.getTagData(Tlv::getTag(0x23, 0x00, 0x00, 0x02));
+
+	BOOST_REQUIRE_EQUAL(buffer.size(), 2); 
+	// 0x00 0x00 0x00 0x01 is data
+	
+	int j = 6; // data start at index 6
+	for(auto i : buffer) {
+		BOOST_CHECK_EQUAL(i, data[j]);
+		++j;
+	}
+}
+
+BOOST_AUTO_TEST_CASE(hex3) {
+	unsigned char data[] = { 0x22, 0x01, 0x01, 0x01, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x02, 0x22, 0x12, 0x11 };
+	Tlv client_id_tag(data, sizeof(data));
+
+	std::vector<unsigned char> buffer = client_id_tag.getTagData(Tlv::getTag(0x22, 0x01, 0x01, 0x01));
+
+	BOOST_REQUIRE_EQUAL(buffer.size(), 8); 
+	// 0x00 0x00 0x00 0x01 is data
+	
+	int j = 6;
+	for(auto i : buffer) {
+		BOOST_CHECK_EQUAL(i, data[j]);
+		++j;
+	}
+}
+
+BOOST_AUTO_TEST_CASE(not_found) {
+	unsigned char input[] = {0, 0, 0, 14, 1, 13, 0, 0, 0, 11, 0, 2, 4, 4, 0, 0, 0, 13, 0, 1, 2};
+	std::vector<unsigned char> output_find_22 = {};
+
+	Tlv o(input, boost::size(input));
+	std::vector<unsigned char> buffer = o.getTagData(22); // returns data without header
+	// there is no data with this tag
+
+	BOOST_REQUIRE_EQUAL(buffer.size(), output_find_22.size());
+}
+
+BOOST_AUTO_TEST_CASE(hex_not_found) {
+	unsigned char input[] = { 0x22, 0x01, 0x01, 0x01, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x02, 0x22, 0x12, 0x11 };
+	std::vector<unsigned char> output = {};
+
+	Tlv o(input, boost::size(input));
+	std::vector<unsigned char> buffer = o.getTagData(127); // returns data without header
+	// there is no data with this tag
+
+	BOOST_REQUIRE_EQUAL(buffer.size(), output.size());
+}
+
 
